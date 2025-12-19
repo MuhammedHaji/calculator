d724bb0f82be30f02e6012389d2d966a883505f9
MuhammedHaji <138349773+MuhammedHaji@users.noreply.github.com>
2025-12-19 18:41:05 +0300

    Make calculator functionl with operators and evaluation

--- script.js
+++ script.js
@@ -28,5 +28,62 @@ function operate(operator,a,b){
     }
 };
 
+//calculator memory ( very important)
 
+let firstNumber="";
+let secondNumber="";
+let currentOperator=null
+let shouldResetDisplay=false;
+
+const display=document.getElementById("display");
+const buttons=document.querySelectorAll("button");
+
+function appendNumber(number){
+    if(display.textContent==="0"||shouldResetDisplay){
+        display.textContent=number;
+        shouldResetDisplay=false;
+    }
+    else{
+        display.textContent +=number;
+    }
+}
+
+function setOperator(operator){
+    if (currentOperator !==null) evaluate();
+    firstNumber=display.textContent;
+    currentOperator=operator;
+    shouldResetDisplay=true;
+}
+
+function evaluate(){
+    if(currentOperator===null || shouldResetDisplay) return;
+    secondNumber=display.textContent;
+
+    let result =operate(currentOperator,firstNumber,secondNumber);
+    display.textContent=Math.round(result*1000)/1000;
+    currentOperator=null;
+    shouldResetDisplay=true;
+}
+//clear button
+function clearAll(){
+    display.textContent="0"
+    firstNumber="";
+    secondNumber="";
+    currentOperator=null;
+    shouldResetDisplay=false;
+}
+//event listeners for buttons
+buttons.forEach(button =>{
+    button.addEventListener("click",()=>{
+        if(!isNaN(button.textContent)){
+            appendNumber(button.textContent)
+        } else if(button.textContent==="C"){
+            clearAll();
+        } else if (button.textContent==="="){
+            evaluate();
+        } else{
+            setOperator(button.textContent)
+        }
+    });
+});
 
